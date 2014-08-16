25004fc8333b305c4c358f0f47dd8eef97b99bda
Thomas Watson Steen <w@tson.dk>
2014-08-17 00:38:20 +0200

    Better tests

--- test.js
+++ test.js
@@ -3,24 +3,20 @@
 var test = require('tape');
 var aar = require('./');
 
-var async = function (callback) {
-  process.nextTick(function () {
-    callback();
-  });
-};
-
 test('should call the callback when all async stuff is done', function (t) {
   var next = aar(function (err, results) {
     t.error(err);
     t.equal(results.length, 2);
+    t.deepEqual(results, [1, 2]);
     t.end();
   });
-  async(next());
-  async(next());
+  process.nextTick(next().bind(null, null, 1));
+  process.nextTick(next().bind(null, null, 2));
 });
 
 test('should pass on arguments to nested callbacks', function (t) {
   var next = aar(function (err, results) {
+    t.equal(results.length, 1);
     t.end();
   });
   var cb = next(function (a1, a2, a3) {
@@ -37,7 +33,23 @@ test('should call the callback with the first error after all async stuff is don
     t.equal(results.length, 2);
     t.end();
   });
-
   setTimeout(next().bind(null, new Error('second')), 20);
   setTimeout(next().bind(null, new Error('first')), 10);
+});
+
+test('should handle multiple async batches', function (t) {
+  var calledNext1 = false;
+  var next1 = aar(function (err, results) {
+    t.error(err);
+    t.equal(results.length, 1);
+    calledNext1 = true;
+  });
+  var next2 = aar(function (err, results) {
+    t.error(err);
+    t.equal(results.length, 1);
+    t.ok(calledNext1);
+    t.end();
+  });
+  setTimeout(next1(), 10);
+  setTimeout(next2(), 20);
 });
